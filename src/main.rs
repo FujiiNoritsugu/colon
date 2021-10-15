caa4844d41a463ca5875d38e5551acb1e68ce919
FujiiNoritsugu <quercus491@gmail.com>
2021-10-15 15:32:34 +0900

    ２つのループを別スレッドで実行する

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 // Version 1.0
+use std::thread;
 use std::time::Instant;
+const MAX_SIZE:i32 = 1000000;
 
 fn main() {
     let start = Instant::now();
-	for p in 0..=1000000{
-		pom(&p);
-	}
+
+    let handle1 = thread::spawn(||{
+        for p in 0..500000{
+            pom(&p);
+        }    
+    });
+
+    let handle2 = thread::spawn(||{
+        for p in 500000..=MAX_SIZE{
+            pom(&p);
+        }    
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
     let end = start.elapsed();
     println!("{}.{:03}秒経過しました。", end.as_secs(), end.subsec_nanos() / 1_000_000); 
 }
